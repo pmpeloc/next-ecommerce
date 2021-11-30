4fd0437ef816b98926c4800ca2488fd65e959a6c
Pablo Misael Peloc <pmpeloc@gmail.com>
2021-11-29 23:52:43 -0300

    feat: TopBar con logo agregado

--- components/Header/TopBar/TopBar.js
+++ components/Header/TopBar/TopBar.js
@@ -1,7 +1,29 @@
+import { Container, Grid, Image, Input } from 'semantic-ui-react';
+import Link from 'next/link';
+
 export const TopBar = () => {
   return (
-    <div>
-      <h1>TopBar...</h1>
+    <div className='top-bar'>
+      <Container>
+        <Grid className='top-bar'>
+          <Grid.Column width={8} className='top-bar__left'>
+            <Logo />
+          </Grid.Column>
+          <Grid.Column width={8} className='top-bar__right'>
+            <h2>Buscador</h2>
+          </Grid.Column>
+        </Grid>
+      </Container>
     </div>
   );
 };
+
+function Logo() {
+  return (
+    <Link href='/'>
+      <a>
+        <Image src='/logo.png' alt='Gamming' />
+      </a>
+    </Link>
+  );
+}
