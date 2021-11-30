26466596618befc412ccb3c92c2fdf085ef66a9d
Pablo Misael Peloc <pmpeloc@gmail.com>
2021-11-30 17:25:40 -0300

    feat: Agregado Formik y Yup al proyecto

--- components/Auth/RegisterForm/RegisterForm.js
+++ components/Auth/RegisterForm/RegisterForm.js
@@ -1,14 +1,54 @@
 import React, { useState } from 'react';
 import { Form, Button } from 'semantic-ui-react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 export default function RegisterForm({ showLoginForm }) {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: (formData) => {
+      console.log(formData);
+    },
+  });
+
   return (
-    <Form className='login-form'>
-      <Form.Input name='name' type='text' placeholder='Nombre' />
-      <Form.Input name='lastname' type='text' placeholder='Apellido' />
-      <Form.Input name='username' type='text' placeholder='Nombre de Usuario' />
-      <Form.Input name='email' type='text' placeholder='Correo Electrónico' />
-      <Form.Input name='password' type='password' placeholder='Contraseña' />
+    <Form className='login-form' onSubmit={formik.handleSubmit}>
+      <Form.Input
+        name='name'
+        type='text'
+        placeholder='Nombre'
+        onChange={formik.handleChange}
+        error={formik.errors.name}
+      />
+      <Form.Input
+        name='lastname'
+        type='text'
+        placeholder='Apellido'
+        onChange={formik.handleChange}
+        error={formik.errors.lastname}
+      />
+      <Form.Input
+        name='username'
+        type='text'
+        placeholder='Nombre de Usuario'
+        onChange={formik.handleChange}
+        error={formik.errors.username}
+      />
+      <Form.Input
+        name='email'
+        type='text'
+        placeholder='Correo Electrónico'
+        onChange={formik.handleChange}
+        error={formik.errors.email}
+      />
+      <Form.Input
+        name='password'
+        type='password'
+        placeholder='Contraseña'
+        onChange={formik.handleChange}
+        error={formik.errors.password}
+      />
       <div className='actions'>
         <Button type='button' basic>
           Iniciar Sesión
@@ -19,4 +59,24 @@ export default function RegisterForm({ showLoginForm }) {
       </div>
     </Form>
   );
+}
+
+function initialValues() {
+  return {
+    name: '',
+    lastname: '',
+    username: '',
+    email: '',
+    password: '',
+  };
+}
+
+function validationSchema() {
+  return {
+    name: Yup.string().required(true),
+    lastname: Yup.string().required(true),
+    username: Yup.string().required(true),
+    email: Yup.string().email(true).required(true),
+    password: Yup.string().required(true),
+  };
 }
